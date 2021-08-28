6b4ff5dbd0534824ba83d21a46410a9282be6fe2
BearishStaff <bearishstaff@gmail.com>
2021-08-28 11:10:39 +0700

    init project

---
+++ src/components/Bearish.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <h2> Bearish Staff </h2>
+  <h3> {{msg}} </h3>
+  <h3> {{s1}} </h3>
+
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            msg : 'Wooooooof',
+            s1 : ' so cold and so far '
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
